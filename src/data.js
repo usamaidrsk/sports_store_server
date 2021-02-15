e9fcc7c1be2ad00b4d70abf4f697ef2ccc051805
Usama Idris <53287480+usamaidrsk@users.noreply.github.com>
2021-02-15 14:10:07 +0300

    initialcommit

---
+++ src/data.js
@@ -0,0 +1,22 @@
+var faker = require("faker");
+var data = [];
+var categories = ["Watersports", "Soccer", "Chess", "Running"];
+faker.seed(100);
+for (let i = 1; i <= 500; i++) {
+    var category = faker.helpers.randomize(categories);
+    data.push({
+    id: i,
+    name: faker.commerce.productName(),
+    category: category,
+    description: `${category}: ${faker.lorem.sentence(3)}`,
+    price: faker.commerce.price()
+    })
+}
+
+module.exports = function () {
+    return { 
+        products: data,
+        categories: [...new Set(data.map(p => p.category))].sort(),
+        orders: []
+    }
+}
---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require("./express");
+const history = require("connect-history-api-fallback");
+const jsonServer = require("json-server");
+const bodyParser = require('body-parser');
+const auth = require("./authMiddleware");
+const router = jsonServer.router("data.json");
+const app = express();
+app.use(bodyParser.json());
+app.use(auth);
+app.use("/api", router);
+app.use(history());
+// app.use("/", express.static("./dist"));
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3500;
+}
+app.listen(port, function () {
+ console.log(`HTTP Server running on port ${port}`);
+});
